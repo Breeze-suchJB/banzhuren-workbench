34106561512eeac4c2d49c6c6996560136495289
Breeze-suchJB <0+Breeze-suchJB@users.noreply.github.com>
2026-08-19 12:08:55 +0800

    同步最新构建（行尾统一）

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 班主任智能工作台 Service Worker */
-const CACHE_NAME = 'banzhuren-workbench-20260819-022915';
+const CACHE_NAME = 'banzhuren-workbench-20260819-110912';
 const ASSETS = [
   './',
   './index.html',
