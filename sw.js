ec78551385033edb93e6ef82b3539bbd78effc95
Breeze-suchJB <0+Breeze-suchJB@users.noreply.github.com>
2026-08-23 18:21:16 +0800

    使用帮助新增腾讯云 CloudBase 绑定步骤

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 班主任智能工作台 Service Worker */
-const CACHE_NAME = 'banzhuren-workbench-20260823-181235';
+const CACHE_NAME = 'banzhuren-workbench-20260823-182100';
 const ASSETS = [
   './',
   './index.html',
