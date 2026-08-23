ffb746448009405049da58e18183713f1c8af524
Breeze-suchJB <0+Breeze-suchJB@users.noreply.github.com>
2026-08-23 20:45:37 +0800

    使用帮助补充免费版免配安全域名说明

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 班主任智能工作台 Service Worker */
-const CACHE_NAME = 'banzhuren-workbench-20260823-203856';
+const CACHE_NAME = 'banzhuren-workbench-20260823-204457';
 const ASSETS = [
   './',
   './index.html',
