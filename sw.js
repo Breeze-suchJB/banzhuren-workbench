1e79c986164696c5b1fc00d34747e130fb506188
Breeze-suchJB <0+Breeze-suchJB@users.noreply.github.com>
2026-08-24 02:42:54 +0800

    CloudBase失败报错附现场直连探测，自动判断网络瞬时抖动还是真不通

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 班主任智能工作台 Service Worker */
-const CACHE_NAME = 'banzhuren-workbench-20260824-023125';
+const CACHE_NAME = 'banzhuren-workbench-20260824-024232';
 const ASSETS = [
   './',
   './index.html',
